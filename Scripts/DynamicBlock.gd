232c86de65f5afd18dbf7c508e1bb469cad85f88
PanosTrak <pnstrak@gmail.com>
2019-08-03 17:23:54 +0300

    Dynamic Platform Added

---
+++ Scripts/DynamicBlock.gd
@@ -0,0 +1,41 @@
+extends StaticBody2D
+
+enum states { FREEZER, SPIKES, JUMPER }
+
+export(states) var starting_state
+
+export(Texture) var freezer_img
+export(Texture) var spikes_img
+export(Texture) var jumper_img
+
+export(int) var jumper_power = 500
+
+onready var state_texture = {
+	0: freezer_img,
+	1: spikes_img,
+	2: jumper_img
+}
+
+onready var state = starting_state 
+
+func _ready():
+	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed('change_dynamic_block'):
+		if state != 2:
+			state += 1
+		else:
+			state = 0
+	$Sprite.set_texture(state_texture[state])
+
+func _on_Area2D_body_entered(body):
+	match state:
+		states.FREEZER:
+			# TODO do something with freezer
+			pass
+		states.SPIKES:
+			# TODO do something with spikes
+			pass
+		states.JUMPER:
+			body.launch(jumper_power)

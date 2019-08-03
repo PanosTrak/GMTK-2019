2ffbaf967e739e18a214819c9cda012a25e46562
PanosTrak <pnstrak@gmail.com>
2019-08-03 03:13:23 +0300

    Refactored Movement System and Change project settings to fit pixelstyle

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,40 +1,50 @@
 extends KinematicBody2D
 
 const MAX_SPEED = 200
-const ACCELERATION = 25
-const GRAVITY = 25
-const JUMP = 400
-
+const ACCELERATION = 20
+const GRAVITY = 15
+const JUMP = -500
 var velocity = Vector2()
 
-func get_input():
-	var action = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+func movement_input():
+
+	# Add gravity
+	velocity.y += GRAVITY
+
+	# Get Input from player
+	var move_pos = Vector2(0,0)
+	if Input.is_action_pressed('move_right'):
+		move_pos.x += 1
+	if Input.is_action_pressed('move_left'):
+		move_pos.x += -1
+	if Input.is_action_pressed('jump') && is_on_floor():
+		move_pos.y += JUMP
 	
-	if Input.is_action_just_pressed("ui_up") && is_on_floor():
-		velocity.y = -JUMP
-	
-	if action != 0 && is_on_floor():
+	if move_pos.x != 0:
 		$AnimatedSprite.play("run")
-		velocity.x += ACCELERATION if action > 0 else -ACCELERATION
-		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
-		
-		$AnimatedSprite.flip_h = action < 0
-	elif !is_on_floor():
+		if move_pos.x == 1:
+			velocity.x = clamp(velocity.x + ACCELERATION, -MAX_SPEED, MAX_SPEED)
+			$AnimatedSprite.flip_h = false
+		else:
+			velocity.x = clamp(velocity.x - ACCELERATION,-MAX_SPEED, MAX_SPEED)
+			print(velocity.x)
+			$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.play("idle")
+		if velocity.x != 0:
+			velocity.x = int(lerp(velocity.x, 0, 0.25))
+
+	if !is_on_floor():
 		if velocity.y < 0:
 			$AnimatedSprite.play("jump")
 		else:
 			$AnimatedSprite.play("fall")
-			
-		velocity.x = lerp(velocity.x, 0, 0.01)
-	else:
-		$AnimatedSprite.play("idle")
-		velocity.x = lerp(velocity.x, 0, 0.3)
 		
-func set_gravity():
-	velocity.y += 10
+	velocity.y += move_pos.y
+	velocity = move_and_slide(velocity, Vector2(0,-1))
+	if velocity != Vector2(0,0):
+		print(velocity)
+	
 
 func _physics_process(delta):
-	set_gravity()
-	get_input()
-	
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	movement_input()

369fec403ff846229796113932c6ef84033831fd
FireDust01 <53581785+FireDust01@users.noreply.github.com>
2019-08-03 15:06:13 +0200

    start animation capture

--- Save_Movement.gd
+++ Save_Movement.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 signal stop
 
+var anim_Sprite
+
 var time = 0
 var states_position = 0
 var positions_position = 0
 var firstinrun = true
 var positions_lenght = 0
+var nextAnimationChange = 0
+var currentState = "idle"
 
 var states = []
 var positions = []
@@ -14,16 +18,16 @@ var positions = []
 func _process(delta):
 	time += delta
 
-func _changeState(state): #saves when a diffrent animation State is triggered #state as String with name from the Animation State
-	states[states_position] = time
-	states_position += 1
-	states[states_position] = state
-	states_position += 1
+func changeState(state): #saves when a diffrent animation State is triggered #state as String with name from the Animation State
+	if currentState != state:
+		states.append(time)
+		states.append(state)
 
 func setPosition(pos): # pos = global_position from Player
 	positions.append(pos)
 
 func getPosition():
+	checkAnimation()
 	var tmp = positions[positions_position]
 	positions_position += 1
 	if positions_position == positions_lenght:
@@ -31,5 +35,26 @@ func getPosition():
 		print("Stop Signal")
 	return tmp
 
+func startGhost():
+	nextAnimationChange = states[0]
+	states_position = 1
+	time = 0
+	setLengthIntern()
+
+func setAnimatedSprite(sprite):
+	anim_Sprite = sprite
+
 func setLengthIntern():
 	positions_lenght = positions.size()
+
+func checkAnimation():
+	if time >= nextAnimationChange:
+		anim_Sprite.play(states[states_position])
+		states_position += 1
+		nextAnimationChange = states[states_position]
+		states_position += 1
+
+
+
+
+
--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -16,6 +16,7 @@ var active = true
 func _ready():
 	save_instance = Save.instance()
 	startingposition = self.position
+	save_instance.setAnimatedSprite($AnimatedSprite)
 
 func get_input():
 	var action = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
@@ -25,6 +26,7 @@ func get_input():
 	
 	if action != 0 && is_on_floor():
 		$AnimatedSprite.play("run")
+		save_instance.changeState("run")
 		velocity.x += ACCELERATION if action > 0 else -ACCELERATION
 		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
 		
@@ -32,12 +34,15 @@ func get_input():
 	elif !is_on_floor():
 		if velocity.y < 0:
 			$AnimatedSprite.play("jump")
+			save_instance.changeState("jump")
 		else:
 			$AnimatedSprite.play("fall")
+			save_instance.changeState("fall")
 			
 		velocity.x = lerp(velocity.x, 0, 0.01)
 	else:
 		$AnimatedSprite.play("idle")
+		save_instance.changeState("idle")
 		velocity.x = lerp(velocity.x, 0, 0.3)
 		
 func set_gravity():
@@ -58,3 +63,4 @@ func _process(delta):
 
 func _on_Area2D_area_entered(area):
 	active = false
+	save_instance.startGhost()

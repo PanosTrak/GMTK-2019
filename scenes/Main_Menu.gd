985e33cb2514b954742fca5825ad4d5aa69a107e
FireDust01 <53581785+FireDust01@users.noreply.github.com>
2019-08-04 00:34:34 +0200

    added level menu

---
+++ scenes/Main_Menu.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+export (PackedScene) var Level_1
+export (PackedScene) var Level_2
+export (PackedScene) var Level_3
+export (PackedScene) var Level_4
+export (PackedScene) var Level_5
+export (PackedScene) var Level_6
+export (PackedScene) var Level_7
+export (PackedScene) var Level_8
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to(Level_1)
+
+
+func _on_level_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()

605cf22a30f95fcd9f1c1843b5eeee272f202b0a
FireDust01 <53581785+FireDust01@users.noreply.github.com>
2019-08-04 18:46:18 +0200

    added new Menu_Main

---
+++ scenes/Menu_Main.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+export (PackedScene) var Level_1
+export (PackedScene) var Level_Menu
+
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to(Level_1)
+
+
+func _on_level_pressed():
+	get_tree().change_scene_to(Level_Menu)
+
+
+func _on_exit_pressed():
+	get_tree().quit()

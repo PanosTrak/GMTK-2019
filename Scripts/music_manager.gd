a8dde11eeb739b1cacbaf641e62235836bcf0255
PanosTrak <pnstrak@gmail.com>
2019-08-04 16:59:00 +0300

    Music Manager Done

--- Scripts/music_manager.gd
+++ Scripts/music_manager.gd
@@ -9,29 +9,39 @@ export(AudioStreamSample) var ghost_five
 export(AudioStreamSample) var ghost_six
 
 onready var audio_to_play = no_ghost
+onready var hud = get_node('../HUD')
+onready var audio_player = $AudioStreamPlayer
 
-var old_ghost_size = 0
+var old_ghost_size
 
 func get_ghost_size():
-	return rand_range(0,6)
+	return clamp(hud.getGhostNumber(), 0, 6)
 
 func _ready():
-	pass
+	audio_player.play()
 
 
 func _process(delta):
-	match get_ghost_size():
-		0:
-			audio_to_play = no_ghost
-		1:
-			audio_to_play = ghost_one
-		2:
-			audio_to_play = ghost_two
-		3:
-			audio_to_play = ghost_three
-		4:
-			audio_to_play = ghost_four
-		5:
-			audio_to_play = ghost_five
-		6:
-			audio_to_play = ghost_six
+	var new_ghost_size = get_ghost_size()
+	if new_ghost_size != old_ghost_size:
+		match new_ghost_size:
+			0:
+				audio_to_play = no_ghost
+			1:
+				audio_to_play = ghost_one
+			2:
+				audio_to_play = ghost_two
+			3:
+				audio_to_play = ghost_three
+			4:
+				audio_to_play = ghost_four
+			5:
+				audio_to_play = ghost_five
+			6:
+				audio_to_play = ghost_six
+		audio_player.set_stream(audio_to_play)
+		audio_player.play()
+		old_ghost_size = new_ghost_size
+
+func _on_AudioStreamPlayer_finished():
+	audio_player.play()

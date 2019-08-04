0be2296caf9ec4f683a0161535108a9e84b42dcf
FireDust01 <53581785+FireDust01@users.noreply.github.com>
2019-08-04 18:04:02 +0200

    added next level in level gd

--- scenes/Levels/Level_8.gd
+++ scenes/Levels/Level_8.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 export var Checkpoints = 3
 export var Name = "Level"
+export (PackedScene) var Next_Level 
 
 var hud
 
@@ -9,3 +10,6 @@ func _ready():
 	hud = get_child(0)
 	hud.setName(Name)
 	hud.setScoreMax(Checkpoints)
+
+func getNextLevel():
+	return Next_Level

12be3a28e61c1592aa55fd0d2c98bece4fd54f8d
FireDust01 <53581785+FireDust01@users.noreply.github.com>
2019-08-04 13:10:35 +0200

    Revert "fixed player/ghost collsion"
    This reverts commit c7022edbd5d861409faa349093b6fe8aca3b4784.

--- Scripts/DynamicBlock.gd
+++ Scripts/DynamicBlock.gd
@@ -62,8 +62,7 @@ func _on_ChangeStateArea2D_body_entered(body):
 			state = 0
 
 func _start_freeze(object):
-	if object.is_in_group('player'):
-		$FreezeTimer.start(freeze_time)
+	$FreezeTimer.start(freeze_time)
 	object.freeze()
 	var freeze_particles_inst = freeze_particles.instance()
 	add_child(freeze_particles_inst)
